c743d5b54359be3adbd61e83b4b5cbd613330a03
pavel <paveltextfree@gmail.com>
2018-04-02 00:21:41 +0300

    change theme to themes obj

--- src/components/Root.js
+++ src/components/Root.js
@@ -2,7 +2,7 @@ import React from 'react'
 import SidebarContainer from '../containers/SidebarContainer'
 import EventsContainer from '../containers/EventsContainer'
 import styled, { ThemeProvider } from 'styled-components'
-import { MAIN_THEME, SECONDARY_THEME } from '../theme'
+import themes from '../themes'
 
 const RootContainer = styled.div`
   display: flex;
@@ -12,7 +12,11 @@ const RootContainer = styled.div`
 const Root = ({ themeName }) => {
   return (
     <ThemeProvider
-      theme={String(themeName) === 'secondary' ? SECONDARY_THEME : MAIN_THEME}
+      theme={
+        String(themeName) === 'secondary'
+          ? themes.SECONDARY_THEME
+          : themes.MAIN_THEME
+      }
     >
       <RootContainer>
         <SidebarContainer />
